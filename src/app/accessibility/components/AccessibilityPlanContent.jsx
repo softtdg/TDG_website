8e019a6317ff3ad93e7dfa3d365ae631d0a68016
softtdg <software@tdgdesign.com>
2025-10-03 17:32:15 +0530

    Add Accessibility page and components; include new HeroSection and AccessibilityPlanContent for improved user experience and compliance with accessibility standards. Add download icon for accessibility plan download feature.

---
+++ src/app/accessibility/components/AccessibilityPlanContent.jsx
@@ -0,0 +1,219 @@
+import React from "react";
+
+export const AccessibilityPlanContent = () => {
+  return (
+    <div className="max-w-[1300px] mx-auto px-4 sm:px-6 md:px-8 py-8 md:py-16 lg:py-[100px] bg-white">
+      {/* Download Header */}
+      <div className="flex flex-row  items-center gap-3 mb-8">
+        <img src="/icons/download.svg" alt="" className="w-10 sm:w-[46px]" />
+        <h2 className="text-2xl sm:text-[30px] font-bold text-[#000000] sm:ms-4 mt-4 sm:mt-0">
+          DOWNLOAD OUR MULTI YEAR ACCESSIBILITY PLAN
+        </h2>
+      </div>
+
+      {/* Introduction */}
+      <div className="mb-10 md:mb-12">
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          TDG Transit Design Group Inc. (“TDG”) is committed to providing goods
+          and services to persons with disabilities in a way that is consistent
+          with the principles of independence, dignity,  integration and equal
+          opportunity. Our goal is to provide all customers and visitors,
+          including those with disabilities, with the same opportunity to access
+          and use TDG’s goods and services. If a barrier to accessing our goods
+          and services cannot be removed, we will seek alternative ways to
+          access the goods and services.
+        </p>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          This Accessible Customer Service Policy applies to the delivery of all
+          goods and services by  TDG in the province of Ontario. This policy
+          applies to all TDG employees who work in Ontario, as well as third
+          parties who provide goods, services or facilities on behalf of TDG.  
+        </p>
+
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          The following policies and practices are intended to meet the
+          requirements of the Accessibility for Ontarians with Disabilities Act,
+          2005 and its Customer Service Standards.
+        </p>
+      </div>
+
+      {/* Communication Section */}
+      <div className="mb-10 md:mb-12">
+        <h3 className="mb-4 py-2 px-3 sm:px-4 flex items-center text-xl sm:text-2xl md:text-[25px] font-bold bg-[#DBE2E7]">
+          <span className="w-1.5 h-1.5 bg-[black] rounded-full mr-2"></span>
+          COMMUNICATION
+        </h3>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          TDG will communicate with customers and visitors with disabilities in
+          ways that take their disability into account. 
+        </p>
+      </div>
+
+      {/* Assistive Devices Section */}
+      <div className="mb-10 md:mb-12">
+        <h3 className="mb-4 py-2 px-3 sm:px-4 flex items-center text-xl sm:text-2xl md:text-[25px] font-bold bg-[#DBE2E7]">
+          <span className="w-1.5 h-1.5 bg-[black] rounded-full mr-2"></span>
+          ASSISTIVE DEVICES
+        </h3>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          Persons with disabilities will be permitted to obtain or use goods or
+          services through the use of their own assistive devices. If an
+          assistive device may pose a risk to the health and safety of the
+          customer or visitor on the premises, TDG will accommodate the customer
+          or visitor by providing an alternative where possible.
+        </p>
+
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          TDG will ensure that our employees are trained and familiar with
+          various assistive devices that may be used by customers or visitors
+          with disabilities when visiting our facilities.
+        </p>
+      </div>
+
+      {/* Service Animals Section */}
+      <div className="mb-10 md:mb-12">
+        <h3 className="mb-4 py-2 px-3 sm:px-4 flex items-center text-xl sm:text-2xl md:text-[25px] font-bold bg-[#DBE2E7]">
+          <span className="w-1.5 h-1.5 bg-[black] rounded-full mr-2"></span>
+          SERVICE ANIMALS
+        </h3>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          TDG welcomes service animals that accompany people with disabilities.
+          Service animals are free to access all areas of our premises that are
+          open to the public except as otherwise disallowed by law.
+        </p>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          If a service animal is excluded from the premises by law, then TDG
+          will ensure that other measures are made available to enable the
+          customer or visitor with a disability to access or use the goods and
+          services.
+        </p>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          TDG will ensure that all staff dealing with the public are properly
+          trained in how to interact with people with disabilities who are
+          accompanied by a service animal.
+        </p>
+      </div>
+
+      {/* Support Persons Section */}
+      <div className="mb-10 md:mb-12">
+        <h3 className="mb-4 py-2 px-3 sm:px-4 flex items-center text-xl sm:text-2xl md:text-[25px] font-bold bg-[#DBE2E7]">
+          <span className="w-1.5 h-1.5 bg-[black] rounded-full mr-2"></span>
+          SUPPORT PERSONS
+        </h3>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          Support persons that accompany someone with a disability are permitted
+          the same access to our facilities as the person they are accompanying.
+          Any person with a disability who is accompanied by a support person
+          will be allowed to enter TDG’s premises with their support person. At
+          no time will a person with a disability who is accompanied by a
+          support person be prevented from having access to their support person
+          while on TDG premises.
+        </p>
+      </div>
+
+      {/* Notice of Temporary Service Disruption Section */}
+      <div className="mb-10 md:mb-12">
+        <h3 className="mb-4 py-2 px-3 sm:px-4 flex items-center text-xl sm:text-2xl md:text-[25px] font-bold bg-[#DBE2E7]">
+          <span className="w-1.5 h-1.5 bg-[black] rounded-full mr-2"></span>
+          NOTICE OF TEMPORARY SERVICE DISRUPTION
+        </h3>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          If any services to accommodate disabled customers or visitors are
+          interrupted in a way that would limit them from gaining access to
+          TDG’s facilities, goods or services, customers or visitors will be
+          notified. Notice of the temporary interruption will be placed on TDG’s
+          website or another conspicuous location, and include information about
+          the reason for the disruption, its anticipated duration and a
+          description of alternative facilities or services, if available.
+        </p>
+      </div>
+
+      {/* Training Section */}
+      <div className="mb-10 md:mb-12">
+        <h3 className="mb-4 py-2 px-3 sm:px-4 flex items-center text-xl sm:text-2xl md:text-[25px] font-bold bg-[#DBE2E7]">
+          <span className="w-1.5 h-1.5 bg-[black] rounded-full mr-2"></span>
+          TRAINING
+        </h3>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          To create awareness and ensure compliance, TDG will provide customer
+          accessibility training to all of its employees who work in Ontario and
+          provide goods or services, and all those who are involved in the
+          development and approvals of customer service policies, practices and
+          procedures.
+        </p>
+        <p className="mb-4 text-lg md:text-[22px] font-semibold">
+          Training will include the following:
+        </p>
+        <ul className="mb-4 text-base sm:text-lg md:text-[20px] list-disc ml-6 md:ml-8 space-y-1">
+          <li className="mb-4">
+            The purposes of the Accessibility for Ontarians with Disabilities
+            Act, 2005 and the requirements of the Customer Service Standard.
+          </li>
+          <li className="mb-4">
+            How to interact and communicate with people with various types of
+            disabilities.
+          </li>
+          <li className="mb-4">
+            How to interact with people with disabilities who use an assistive
+            device or require the assistance of a service animal or support
+            person.
+          </li>
+          <li className="mb-4">
+            How to use personal assistive devices on-premises to help with the
+            provision of goods or services to people with disabilities.
+          </li>
+          <li className="mb-4">
+            What to do if a person with a disability is having difficulty
+            accessing TDG’s goods and services.
+          </li>
+          <li className="mb-4">
+            TDG’s policies, practices and procedures relating to accessible
+            customer service.
+          </li>
+        </ul>
+        <p className="mb-4 mt-3 text-base sm:text-lg md:text-[20px]">
+          Training will be provided during orientation and on an ongoing basis
+          when changes are made to these policies, practices and procedures.
+        </p>
+      </div>
+
+      {/* Feedback Section */}
+      <div className="mb-10 md:mb-12">
+        <h3 className="mb-4 py-2 px-3 sm:px-4 flex items-center text-xl sm:text-2xl md:text-[25px] font-bold bg-[#DBE2E7]">
+          <span className="w-1.5 h-1.5 bg-[black] rounded-full mr-2"></span>
+          FEEDBACK
+        </h3>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          All documents relating to accessible customer service will be made
+          available, upon request, in a format that takes into account the
+          customer’s disability needs.
+        </p>
+        <p className="mb-4 text-base sm:text-lg md:text-[20px]">
+          Customers and visitors may provide feedback about the accessibility of
+          our goods, services and facilities by contacting TDG’s Manager of
+          Human Resources using one of the following methods:
+        </p>
+        <div className="space-y-4 sm:space-y-5">
+          <p className="text-base sm:text-lg md:text-[20px]">
+            <strong>Phone:</strong> 905-608-9539
+          </p>
+          <p className="text-base sm:text-lg md:text-[20px]">
+            <strong>Fax:</strong> 905-608-9984
+          </p>
+          <p className="text-base sm:text-lg md:text-[20px]">
+            <strong>Email:</strong> accessibility@tdgdesign.com
+          </p>
+          <p className="text-base sm:text-lg md:text-[20px]">
+            <strong>Mail:</strong> TDG Transit Design Group Inc.
+          </p>
+        </div>
+        <p className="mt-2 text-base sm:text-lg md:text-[20px]">
+          9-3770A Laird Road, Mississauga, Ontario, L5L 0A7
+        </p>
+        <p className="mt-2 text-base sm:text-lg md:text-[20px]">
+          TDG will respond to feedback requests within 15 business days.
+        </p>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/accessibility/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { HeroSection } from "./components/HeroSection";
+import { AccessibilityPlanContent } from "./components/AccessibilityPlanContent";
+
+const page = () => {
+  return (
+    <div>
+      <HeroSection />
+      <AccessibilityPlanContent />
+    </div>
+  );
+};
+
+export default page;
